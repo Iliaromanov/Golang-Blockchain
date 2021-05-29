c2069cc07cd81b547774b97d818e5bfe1926ae18
Iliaromanov <iliaromanov033@gmail.com>
2021-05-29 15:57:54 -0400

    transaction struct and related functions

---
+++ database/transaction.go
@@ -0,0 +1,22 @@
+package database
+
+type Account string
+
+func newAccount(value string) Account {
+	return Account(value)
+}
+
+type Transaction struct {
+	From  Account `json:"from"` // JSON encoding struct tag to ensure field name 'From' in Json output
+	To    Account `json:"to"`
+	Value uint	  `json:"value"`
+	Data  string  `json:"data"`	
+}
+
+func newTransaction(from Account, to Account, value uint, data string) Transaction {
+	return Transaction{from, to, value, data}
+}
+
+func isReward(t Transaction) bool {
+	return t.Data == "reward"
+}
