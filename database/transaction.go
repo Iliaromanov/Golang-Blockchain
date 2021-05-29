c9f3b915e875998b75657429709a84f92aa17ae3
Iliaromanov <iliaromanov033@gmail.com>
2021-05-29 19:34:19 -0400

    capitilizing program scoped functions

--- database/transaction.go
+++ database/transaction.go
@@ -2,24 +2,24 @@ package database
 
 type Account string
 
-func newAccount(value string) Account {
+func NewAccount(value string) Account {
 	return Account(value)
 }
 
 type Transaction struct {
-	From  Account `json:"from"` // JSON encoding struct tag to ensure field name 'From' in Json output
+	From  Account `json:"from"` // JSON encoding struct tag to ensure field name 'From' in JSON file
 	To    Account `json:"to"`
 	Value uint    `json:"value"`
 	Data  string  `json:"data"`	
 }
 
 // Creates a new transaction
-func newTransaction(from Account, to Account, value uint, data string) Transaction {
+func NewTransaction(from Account, to Account, value uint, data string) Transaction {
 	return Transaction{from, to, value, data}
 }
 
 // Checks if the transaction was a reward
 // (creator of blockchain {me} gets a weekly reward amount of tokens for my brilliant idea)
-func isReward(t Transaction) bool {
+func IsReward(t Transaction) bool {
 	return t.Data == "reward"
 }
