af597311b8c7fee754aaee108f9151d42b7c6ff1
Iliaromanov <iliaromanov033@gmail.com>
2021-05-29 20:21:33 -0400

    finished NewStateFromDisk(); parsing state.json() and returning current state struct

--- database/state.go
+++ database/state.go
@@ -31,5 +31,32 @@ func NewStateFromDisk() (*State, error) {
 	for account, balance := range gen.Balances {
 		balances[account] = balance
 	}
-	
+
+	// Updating genesis State balances by sequentially 
+	//  replaying all database events from transactions.db
+	transactionDbFilePath := filepath.Join(cwd, "database", "transaction.db")
+	file, err := os.OpenFile(transactionDbFilePath, os.O_APPEND|os.O_RDWR, 0600) // 0600 is readable+writable permission
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(file)
+	state := &State{balances, make([]Transaction, 0), file}
+
+	// Iterate of transaction.db line by line
+	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			return nil, err
+		}
+
+		var transaction Transaction
+		json.Unmarshal(scanner.Bytes(), &transaction) // parses state.json line into transaction
+
+		// Add transaction to state
+		if err := state.apply(transaction); err != nil { // should update balances map and append to transactions slice
+			return nil, err
+		}
+	}
+
+	return state, nil
 }
