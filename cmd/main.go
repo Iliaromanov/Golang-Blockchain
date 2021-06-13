4febb64f39f7f74b33549b06376238dc77300617
Iliaromanov <iliaromanov033@gmail.com>
2021-06-13 17:49:24 -0400

    root and version cmds

--- cmd/main.go
+++ cmd/main.go
@@ -6,15 +6,27 @@ import (
 	"os"
 )
 
-func main() {
-	// This command will serve as the programs entry point.
-	var tblCmd = &cobra.Command{
+/**/
+var (
+	tblCmd = &cobra.Command{ // This command will serve as the programs entry point.
 		Use:   "tbl",
 		Short: "The Blockchain Ledger CLI",
-		Run: func(cmd *cobra.Command, args []string) {
-		},
 	}
 
+	versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "Describes version.",
+		Run: func (cmd *cobra.Command, args []string) {
+			fmt.Println("Version 0.1.0-beta")
+		},
+	}
+)
+
+func init() {
+	tblCmd.AddCommand(versionCmd)
+}
+
+func main() {
 	if err := tblCmd.Execute(); err != nil { // Execute root command and check for errors
 		fmt.Fprintln(os.Stderr, err) // Print error to stderr instead of stdio
 		os.Exit(1) // exit with code 1 (failed)
